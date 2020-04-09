63143bb6f0e0d341af643b461f028641d89949fb
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2020-04-09 10:08:21 -0400

    Add .zlogout

--- setup.zsh
+++ setup.zsh
@@ -8,8 +8,11 @@ if [ ! -x /usr/local/bin/brew ]; then
     echo
 fi
 
-brew install zsh
+# brew install zsh
 
 # Link
 mv "${HOME}/.zshrc" "${HOME}/.zshrc.${BACKUP_SUFFIX}"
-ln -s "${DOTFILES}/zsh/.zshrc" "${HOME}/.zshrc"
+ln -s "${DOTFILES}/zsh/zshrc" "${HOME}/.zshrc"
+
+mv "${HOME}/.zlogout" "${HOME}/.zlogout.${BACKUP_SUFFIX}"
+ln -s "${DOTFILES}/zsh/zlogout" "${HOME}/.zlogout"
